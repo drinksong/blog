a1186d7ad4fd59eda77b37d9ca930a4c8e05aba5
songjunke <songjunke@baidu.com>
2018-09-28 16:54:10 +0800

    remove dist & add .gitignore

---
+++ README.md
@@ -0,0 +1,30 @@
+# Introduction
+
+A blog by VuePress
+
+> VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
+
+[官网](https://vuepress.vuejs.org/)
+[中文版](https://vuepress.vuejs.org/zh/)
+
+# Getting started
+
+```
+yarn install
+```
+
+
+# Dev
+
+```
+yarn run dev
+```
+
+# Build
+
+```
+yarn run build
+```
+
+# License
+MIT
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+    title: 'Drink`s blog',
+    description: 'Learn young, learn fair.',
+    base: '/blog/',
+    themeConfig: {
+        // 假定 GitHub。也可以是一个完整的 GitLab 网址
+        repo: 'https://github.com/drinksong/blog',
+        nav: [
+            {
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'Article',
+                link: '/article/'
+            },
+            {
+                text: 'About me',
+                link: '/about/'
+            }
+        ],
+        sidebar: {
+            '/article/': [
+                '',
+                'react-context',
+                'react-hoc',
+                'shell-tut'
+            ]
+        }
+    }
+}
+
+function genSidebarConfig (title) {
+    return [
+      {
+        title,
+        collapsable: false,
+        children: [
+          '',
+          'test'
+        ]
+      }
+    ]
+  }
